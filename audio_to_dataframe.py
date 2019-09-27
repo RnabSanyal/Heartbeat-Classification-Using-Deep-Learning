410f1a84b03ff85e8d6f33f3f619c7718c5b214a
RnabSanyal <=>
2019-09-28 00:50:38 +0530

    Audio to Dataframe

---
+++ audio_to_dataframe.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+import re
+import librosa
+
+
+DATA_PATH = ".\\all_final\\"
+
+FILES = os.listdir(DATA_PATH)
+
+data = []
+
+for file in FILES:
+	path = DATA_PATH + file
+
+	audio, sr = librosa.load(path)
+
+	data.append([audio, sr, len(audio), re.match('\D+', file).group()])
+
+	print("processed " + file + " @" + str(sr))
+
+heartbeats = pd.DataFrame(data, columns = ['audio', 'sampling_rate', 'length', 'label'])
+print(heartbeats.describe())
+
+heartbeats.to_hdf('audio_df.h5', 'raw')
